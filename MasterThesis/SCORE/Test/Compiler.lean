64832970d4d8bd3e7e7a1f12e43e4628a996c8d8
andreadlm <delmastro2000@gmail.com>
2024-05-17 17:04:40 +0200

    Test inversione

--- MasterThesis/SCORE/Test/Compiler.lean
+++ MasterThesis/SCORE/Test/Compiler.lean
@@ -3,6 +3,9 @@ import MasterThesis.SCORE.Compiler
 
 open SCORE
 
-def store0 : SCORE.store := ["x" ↦ [0]] ["y" ↦ [3]] ["z" ↦ [0]]
+def store0 : store := ["x" ↦ [0]] ["y" ↦ [3]] ["z" ↦ [0]]
 
-#eval (eval (LOOP2SCORE LOOP.func.pred) store0) "x"
+def storePred : store := (eval (LOOP2SCORE LOOP.func.pred) store0)
+#eval  storePred "x"
+def storePredᵢ : store := (evalI (LOOP2SCORE LOOP.func.pred) storePred)
+#eval storePredᵢ "y"
