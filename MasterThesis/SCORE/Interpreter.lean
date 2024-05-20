5e0bf446c97285af5429692c5f943caf151408ac
andreadlm <delmastro2000@gmail.com>
2024-05-20 12:44:02 +0200

    Notazione

--- MasterThesis/SCORE/Interpreter.lean
+++ MasterThesis/SCORE/Interpreter.lean
@@ -6,35 +6,35 @@ namespace SCORE
 open SCORE.com
 
 mutual
-  def eval (P : com) (s : store) : store :=
+  def eval (P : com) (σ : store) : store :=
     match P with
-    | SKIP    => s
-    | CON x   => [x ↦ (0 :: (s x))] s
-    | NOC x   => match s x with
-                 | 0 :: t => [x ↦ t] s
-                 | _      => s -- CHECK: error?
-    | DEC x   => [x ↦ (((s x).head! - 1) :: (s x).tail!)] s
-    | INC x   => [x ↦ (((s x).head! + 1) :: (s x).tail!)] s
-    | SEQ P Q => (eval Q) (eval P s)
-    | FOR x P => match (s x).head! with
-                 | Int.ofNat   v => Nat.iterate (fun s' => eval P s') v s
-                 | Int.negSucc v => Nat.iterate (fun s' => evalI P s') (Nat.succ v) s
+    | SKIP    => σ
+    | CON x   => [x ↦ (0 :: (σ x))] σ
+    | NOC x   => match σ x with
+                 | 0 :: t => [x ↦ t] σ
+                 | _      => σ -- CHECK: error?
+    | DEC x   => [x ↦ (((σ x).head! - 1) :: (σ x).tail!)] σ
+    | INC x   => [x ↦ (((σ x).head! + 1) :: (σ x).tail!)] σ
+    | SEQ P Q => (eval Q) (eval P σ)
+    | FOR x P => match (σ x).head! with
+                 | Int.ofNat   v => (fun τ  => eval P τ)^[v] σ
+                 | Int.negSucc v => (fun τ => evalI P τ)^[v.succ] σ
 
-  def evalI (P : com) (s : store) : store :=
+  def evalI (P : com) (σ : store) : store :=
     match P with
-    | SKIP    => s
-    | CON x   => match s x with
-                 | 0 :: t => [x ↦ t] s
-                 | _      => s -- CHECK: error?
-    | NOC x   => [x ↦ (0 :: (s x))] s
-    | DEC x   => [x ↦ (((s x).head! + 1) :: (s x).tail!)] s
-    | INC x   => [x ↦ (((s x).head! - 1) :: (s x).tail!)] s
-    | SEQ P Q => (evalI P) (evalI Q s)
-    | FOR x P => match (s x).head! with
-                 | Int.ofNat   v => Nat.iterate (fun s' => evalI P s') v s
-                 | Int.negSucc v => Nat.iterate (fun s' => eval P s') (Nat.succ v) s
+    | SKIP    => σ
+    | CON x   => match σ x with
+                 | 0 :: t => [x ↦ t] σ
+                 | _      => σ -- CHECK: error?
+    | NOC x   => [x ↦ (0 :: (σ x))] σ
+    | DEC x   => [x ↦ (((σ x).head! + 1) :: (σ x).tail!)] σ
+    | INC x   => [x ↦ (((σ x).head! - 1) :: (σ x).tail!)] σ
+    | SEQ P Q => (evalI P) (evalI Q σ)
+    | FOR x P => match (σ x).head! with
+                 | Int.ofNat   v => (fun τ => evalI P τ)^[v] σ
+                 | Int.negSucc v => (fun τ => eval P τ)^[v.succ] σ
 end
 
-theorem inv_evalI (P : com) (s : store): eval (inv P) s = evalI P s := sorry
+theorem inv_evalI (P : com) (σ : store) : eval (inv P) σ = evalI P σ := sorry
 
 end SCORE
