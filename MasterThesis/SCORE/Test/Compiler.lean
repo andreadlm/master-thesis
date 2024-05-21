99b8623089bfd328e9308d4cb118ba022ad436b0
andreadlm <delmastro2000@gmail.com>
2024-05-21 15:00:06 +0200

    Nuovo nome funzione di compilazione

--- MasterThesis/SCORE/Test/Compiler.lean
+++ MasterThesis/SCORE/Test/Compiler.lean
@@ -5,7 +5,7 @@ open SCORE
 
 def store0 : store := ["x" ↦ [0]] ["y" ↦ [3]] ["z" ↦ [0]]
 
-def storePred : store := (eval (LOOP2SCORE LOOP.func.pred) store0)
+def storePred : store := (eval (L2S LOOP.func.pred) store0)
 #eval  storePred "x"
-def storePredᵢ : store := (evalI (LOOP2SCORE LOOP.func.pred) storePred)
+def storePredᵢ : store := (evalI (L2S LOOP.func.pred) storePred)
 #eval storePredᵢ "y"
