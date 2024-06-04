c30ec67daade45b3412cdcca714ed3fd9b484a8e
andreadlm <delmastro2000@gmail.com>
2024-06-04 10:45:01 +0200

    Completati lemmi for_inc e for_dec con sorry in iter_dec

--- MasterThesis/SCORE/Compiler_v2.lean
+++ MasterThesis/SCORE/Compiler_v2.lean
@@ -23,7 +23,17 @@ def L2S' (ev : ident) (Lc: LOOP.com) : SCORE.com :=
 
 namespace L2S'
 
-lemma inc_iter {x : ident} {σ: SCORE.store} {k : Int} (v : ℕ) : (σ x).head! = k → (fun τ ↦ eval (INC x) τ)^[v] σ = [x ↦ ((k + Int.ofNat v) :: (σ x).tail!)]σ := by
+example : - Int.negSucc u = Int.ofNat u.succ := by simp [Int.negSucc_coe]
+
+-- TODO: sostituire con un teorema più generale sulla reversibilità?
+lemma eval_inc_dec_evalI {x : ident} {σ : SCORE.store} : eval (INC x) σ = evalI (DEC x) σ := by
+  rw [SCORE.eval, SCORE.evalI]
+
+-- TODO: sostituire con un teorema più generale sulla reversibilità?
+lemma eval_dec_inc_evalI {x : ident} {σ : SCORE.store} : eval (DEC x) σ = evalI (INC x) σ := by
+  rw [SCORE.eval, SCORE.evalI]
+
+lemma iter_inc {x : ident} {σ : SCORE.store} {k : Int} (v : ℕ) : (σ x).head! = k → (fun τ ↦ eval (INC x) τ)^[v] σ = [x ↦ ((k + Int.ofNat v) :: (σ x).tail!)]σ := by
   intros
   induction v generalizing σ with
   | zero      =>
@@ -31,7 +41,7 @@ lemma inc_iter {x : ident} {σ: SCORE.store} {k : Int} (v : ℕ) : (σ x).head! 
       { simp }; rw [this]
     have : (k + Int.ofNat 0) = k := by
       { simp }; rw [this]
-    apply SCORE.store.update_unchanged_cons ‹(σ x).head! = k›
+    exact SCORE.store.update_unchanged_cons ‹(σ x).head! = k›
   | succ m ih =>
     have : (fun τ ↦ eval (INC x) τ)^[m + 1] σ = eval (INC x) ((fun τ ↦ eval (INC x) τ)^[m] σ) := by
       { simp [Nat.add_comm m 1, Function.iterate_add_apply] }; rw[this]
@@ -56,7 +66,7 @@ lemma inc_iter {x : ident} {σ: SCORE.store} {k : Int} (v : ℕ) : (σ x).head! 
       have : ([x ↦ ((k + Int.ofNat (m + 1)) :: (σ x).tail!)]σ) y = σ y := by
         { simp [‹x ≠ y›] }; rw [this]
 
-lemma inc_iter_inv {x : ident} {σ: SCORE.store} {k : Int} (v : ℕ) : (σ x).head! = k → (fun τ ↦ evalI (INC x) τ)^[v] σ = [x ↦ ((k + Int.negSucc (v - 1)) :: (σ x).tail!)]σ := sorry
+lemma iter_dec {x : ident} {σ: SCORE.store} {k : Int} (v : ℕ) : (σ x).head! = k → (fun τ ↦ eval (DEC x) τ)^[v] σ = [x ↦ ((k - Int.ofNat v) :: (σ x).tail!)]σ := sorry
 
 lemma for_inc {x y : ident} {v₁ v₂ : Int} {τ : SCORE.store} : (τ x).head! = v₁ → (τ y).head! = v₂ → eval (FOR y (INC x)) τ = [x ↦ ((v₁ + v₂) :: (τ x).tail!)]τ := by
   intros
@@ -65,13 +75,31 @@ lemma for_inc {x y : ident} {v₁ v₂ : Int} {τ : SCORE.store} : (τ x).head! 
   case h_1 v _ /- (τ y).head! >= 0 -/ =>
     have : v₂ = Int.ofNat v := by
       { rw [← ‹(τ y).head! = v₂›, ‹(τ y).head! = Int.ofNat v›] }; rw [this]
-    apply inc_iter v ‹(τ x).head! = v₁›
-  case h_2 u _ /- (τ y).head! < 0  -/ =>
+    exact iter_inc v ‹(τ x).head! = v₁›
+  case h_2 u _ /- (τ y).head! < 0 -/  =>
+    simp only [← eval_dec_inc_evalI]
     have : v₂ = Int.negSucc u := by
       { rw [← ‹(τ y).head! = v₂›, ‹(τ y).head! = Int.negSucc u›] }; rw [this]
-    apply inc_iter_inv u.succ ‹(τ x).head! = v₁›
+    have : v₁ + Int.negSucc u = v₁ - Int.ofNat (u.succ) := by
+      { simp [Int.negSucc_coe]; linarith }; rw [this]
+    exact iter_dec u.succ ‹(τ x).head! = v₁›
 
-lemma for_dec (x y : ident) (v₁ v₂ : Int) (τ : SCORE.store) : (τ x).head! = v₁ → (τ y).head! = v₂ → eval (FOR y (DEC x)) τ = [x ↦ ((v₁ - v₂) :: (τ x).tail!)]τ := sorry
+lemma for_dec (x y : ident) (v₁ v₂ : Int) (τ : SCORE.store) : (τ x).head! = v₁ → (τ y).head! = v₂ → eval (FOR y (DEC x)) τ = [x ↦ ((v₁ - v₂) :: (τ x).tail!)]τ := by
+  intros
+  rw [SCORE.eval]
+  split
+  case h_1 v _ /- (τ y).head! >= 0 -/ =>
+    have : v₂ = Int.ofNat v := by
+      { rw [← ‹(τ y).head! = v₂›, ‹(τ y).head! = Int.ofNat v›] }; rw [this]
+    exact iter_dec v ‹(τ x).head! = v₁›
+  case h_2 u _ /- (τ y).head! < 0 -/  =>
+    simp only [← eval_inc_dec_evalI]
+    have : v₂ = Int.negSucc u := by
+      { rw [← ‹(τ y).head! = v₂›, ‹(τ y).head! = Int.negSucc u›] }; rw [this]
+    have : v₁ - Int.negSucc u = v₁ + Int.ofNat u.succ := by
+      { simp [Int.negSucc_coe]; linarith }; rw [this]
+    exact iter_inc u.succ ‹(τ x).head! = v₁›
+
 
 lemma evaluation_invariant (x y ev : ident) (τ : SCORE.store) : x ≠ ev → y ≠ ev → (τ x).head! = v₁ → (τ y).head! = v₂ → (τ ev).head! = v₃ → (τ ev) = ((eval (L2S' ev (LOOP.com.ASN x y)) τ) ev) := by
   intros
@@ -95,39 +123,6 @@ lemma evaluation_invariant (x y ev : ident) (τ : SCORE.store) : x ≠ ev → y 
 
 lemma evaluation_zero (x y ev : ident) (τ : SCORE.store) : (τ ev).head! = 0 → ((eval (L2S' ev (LOOP.com.ASN x y)) τ) ev).head! = 0 := by
   sorry -- intro; simpa only [← evaluation_invariant x y ev τ]
-
-/- lemma iterate_lemma (x : ident) (σ γ : SCORE.store) (k : Int) (v : ℕ) : (σ x).head! = k -> (fun τ ↦ eval (INC x) τ)^[v] σ = γ -> (γ x).head! = v + k := by
-  intros h1 h2
-  induction v generalizing γ with
-  | zero =>
-    rw[← h1]
-    simp
-    simp at h2
-    rw[h2]
-  | succ m ih =>
-    simp only [Nat.add_comm m 1, Function.iterate_add_apply] at h2
-    simp at h2 -/
-
-
-
-/- lemma for_inc (x y : ident) (v₁ v₂ : Int) (τ : SCORE.store) : (τ x).head! = v₁ → (τ y).head! = v₂ → eval (FOR y (INC x)) τ = [x ↦ ((v₁ + v₂) :: (τ x).tail!)]τ := by
-  intros
-  rewrite [SCORE.eval]
-  split
-  case h_1 xv1 yv2 xisInt v vPos =>
-    clear vPos
-    induction v with
-    | zero =>
-      simp
-      funext
-      sorry
-    | succ m ih =>
-      simp only [Nat.add_comm m 1, Function.iterate_add_apply]
-      rewrite [ih]
-      simp
-      rewrite [SCORE.eval]
-      simp [List.head!]
-  case h_2 _ v => sorry -/
 
 end L2S'
 
