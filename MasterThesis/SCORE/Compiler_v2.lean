9bf03134a3c6b17c2b3ba78f73d65f4e8b8ba329
andreadlm <delmastro2000@gmail.com>
2024-06-06 19:13:56 +0200

    Continuazione ev_invariant

--- MasterThesis/SCORE/Compiler_v2.lean
+++ MasterThesis/SCORE/Compiler_v2.lean
@@ -3,6 +3,7 @@ import MasterThesis.SCORE.Interpreter
 import MasterThesis.LOOP.Language
 import MasterThesis.LOOP.Interpreter
 import Mathlib.Tactic.Linarith
+import Mathlib.Data.List.Basic
 
 namespace SCORE
 
@@ -33,7 +34,7 @@ lemma eval_inc_dec_evalI {x : ident} {σ : SCORE.store} : eval (INC x) σ = eval
 lemma eval_dec_inc_evalI {x : ident} {σ : SCORE.store} : eval (DEC x) σ = evalI (INC x) σ := by
   rw [SCORE.eval, SCORE.evalI]
 
-lemma iter_inc {x : ident} {σ : SCORE.store} {k : Int} (v : ℕ) : (σ x).head! = k → (fun τ ↦ eval (INC x) τ)^[v] σ = [x ↦ ((k + Int.ofNat v) :: (σ x).tail!)]σ := by
+lemma iter_inc {x : ident} {σ : SCORE.store} {k : Int} (v : ℕ) : (σ x).head! = k → (fun τ ↦ eval (INC x) τ)^[v] σ = [x ↦ ((k + Int.ofNat v) :: (σ x).tail)]σ := by
   intros
   induction v generalizing σ with
   | zero      =>
@@ -46,29 +47,29 @@ lemma iter_inc {x : ident} {σ : SCORE.store} {k : Int} (v : ℕ) : (σ x).head!
     have : (fun τ ↦ eval (INC x) τ)^[m + 1] σ = eval (INC x) ((fun τ ↦ eval (INC x) τ)^[m] σ) := by
       { simp [Nat.add_comm m 1, Function.iterate_add_apply] }; rw[this]
     rw [ih ‹(σ x).head! = k›, SCORE.eval, SCORE.store.update_shrink]
-    have : (([x ↦ ((k + Int.ofNat m) :: (σ x).tail!)]σ) x).head! = (k + Int.ofNat m) := by
+    have : (([x ↦ ((k + Int.ofNat m) :: (σ x).tail)]σ) x).head! = (k + Int.ofNat m) := by
       { simp [List.head!] }; rw [this]
-    have : (([x ↦ ((k + Int.ofNat m) :: (σ x).tail!)]σ) x).tail! = (σ x).tail! := by
+    have : (([x ↦ ((k + Int.ofNat m) :: (σ x).tail)]σ) x).tail = (σ x).tail := by
       { simp }; rw [this]
     funext y
     cases eq_or_ne x y with
     | inl =>
       rw [← ‹x = y›]
-      have : ([x ↦ ((k + Int.ofNat m + 1) :: (σ x).tail!)]σ) x = ((k + Int.ofNat m + 1) :: (σ x).tail!) := by
+      have : ([x ↦ ((k + Int.ofNat m + 1) :: (σ x).tail)]σ) x = ((k + Int.ofNat m + 1) :: (σ x).tail) := by
         { simp }; rw [this]
-      have : ([x ↦ ((k + Int.ofNat (m + 1)) :: (σ x).tail!)]σ) x = ((k + (Int.ofNat m + 1)) :: (σ x).tail!) := by
+      have : ([x ↦ ((k + Int.ofNat (m + 1)) :: (σ x).tail)]σ) x = ((k + (Int.ofNat m + 1)) :: (σ x).tail) := by
         { simp }; rw [this]
       have : (k + Int.ofNat m + 1) = (k + (Int.ofNat m + 1)) := by
         { linarith }; rw [this]
     | inr =>
-      have : ([x ↦ ((k + Int.ofNat m + 1) :: (σ x).tail!)]σ) y = σ y := by
+      have : ([x ↦ ((k + Int.ofNat m + 1) :: (σ x).tail)]σ) y = σ y := by
         { simp [‹x ≠ y›] }; rw [this]
-      have : ([x ↦ ((k + Int.ofNat (m + 1)) :: (σ x).tail!)]σ) y = σ y := by
+      have : ([x ↦ ((k + Int.ofNat (m + 1)) :: (σ x).tail)]σ) y = σ y := by
         { simp [‹x ≠ y›] }; rw [this]
 
-lemma iter_dec {x : ident} {σ: SCORE.store} {k : Int} (v : ℕ) : (σ x).head! = k → (fun τ ↦ eval (DEC x) τ)^[v] σ = [x ↦ ((k - Int.ofNat v) :: (σ x).tail!)]σ := sorry
+lemma iter_dec {x : ident} {σ: SCORE.store} {k : Int} (v : ℕ) : (σ x).head! = k → (fun τ ↦ eval (DEC x) τ)^[v] σ = [x ↦ ((k - Int.ofNat v) :: (σ x).tail)]σ := sorry
 
-lemma for_inc {x y : ident} {v₁ v₂ : Int} {τ : SCORE.store} : (τ x).head! = v₁ → (τ y).head! = v₂ → eval (FOR y (INC x)) τ = [x ↦ ((v₁ + v₂) :: (τ x).tail!)]τ := by
+lemma for_inc {x y : ident} {v₁ v₂ : Int} {τ : SCORE.store} : (τ x).head! = v₁ → (τ y).head! = v₂ → eval (FOR y (INC x)) τ = [x ↦ ((v₁ + v₂) :: (τ x).tail)]τ := by
   intros
   rw [SCORE.eval]
   split
@@ -84,7 +85,7 @@ lemma for_inc {x y : ident} {v₁ v₂ : Int} {τ : SCORE.store} : (τ x).head! 
       { simp [Int.negSucc_coe]; linarith }; rw [this]
     exact iter_dec u.succ ‹(τ x).head! = v₁›
 
-lemma for_dec {x y : ident} {v₁ v₂ : Int} {τ : SCORE.store} : (τ x).head! = v₁ → (τ y).head! = v₂ → eval (FOR y (DEC x)) τ = [x ↦ ((v₁ - v₂) :: (τ x).tail!)]τ := by
+lemma for_dec {x y : ident} {v₁ v₂ : Int} {τ : SCORE.store} : (τ x).head! = v₁ → (τ y).head! = v₂ → eval (FOR y (DEC x)) τ = [x ↦ ((v₁ - v₂) :: (τ x).tail)]τ := by
   intros
   rw [SCORE.eval]
   split
@@ -100,34 +101,33 @@ lemma for_dec {x y : ident} {v₁ v₂ : Int} {τ : SCORE.store} : (τ x).head! 
       { simp [Int.negSucc_coe]; linarith }; rw [this]
     exact iter_inc u.succ ‹(τ x).head! = v₁›
 
-
 theorem ev_invariant {x y ev : ident} {τ : SCORE.store} : x ≠ ev → y ≠ ev → (τ x).head! = v₁ → (τ y).head! = v₂ → (τ ev).head! = 0 → (τ ev) = ((eval (L2S' ev (LOOP.com.ASN x y)) τ) ev) := by
   intros
   rw [L2S', eval, eval, eval]
   rw [for_inc ‹(τ ev).head! = 0› ‹(τ y).head! = v₂›, zero_add]
   rw [eval]
-  have : ([ev ↦ (v₂ :: (τ ev).tail!)]τ) x = τ x := by
+  have : ([ev ↦ (v₂ :: (τ ev).tail)]τ) x = τ x := by
     { simp [‹x ≠ ev›.symm] }; rw [this]
-  have head_x : (([x ↦ (0 :: τ x)][ev ↦ (v₂ :: (τ ev).tail!)]τ) x).head! = 0 := by
+  have head_x : (([x ↦ (0 :: τ x)][ev ↦ (v₂ :: (τ ev).tail)]τ) x).head! = 0 := by
     simp [List.head!]
-  have head_ev : (([x ↦ (0 :: τ x)][ev ↦ (v₂ :: (τ ev).tail!)]τ) ev).head! = v₂ := by
+  have head_ev : (([x ↦ (0 :: τ x)][ev ↦ (v₂ :: (τ ev).tail)]τ) ev).head! = v₂ := by
     simp [‹x ≠ ev›, List.head!]
   rw [for_inc head_x head_ev, zero_add]
-  have : (([x ↦ (0 :: τ x)][ev ↦ (v₂ :: (τ ev).tail!)]τ) x).tail! = τ x := by
+  have : (([x ↦ (0 :: τ x)][ev ↦ (v₂ :: (τ ev).tail)]τ) x).tail = τ x := by
     { simp }; rw [this]
   rw [SCORE.store.update_shrink]
-  have head_x : (([x ↦ (v₂ :: τ x)][ev ↦ (v₂ :: (τ ev).tail!)]τ) x).head! = v₂ := by
+  have head_x : (([x ↦ (v₂ :: τ x)][ev ↦ (v₂ :: (τ ev).tail)]τ) x).head! = v₂ := by
     simp [List.head!]
-  have head_ev : (([x ↦ (v₂ :: τ x)][ev ↦ (v₂ :: (τ ev).tail!)]τ) ev).head! = v₂ := by
+  have head_ev : (([x ↦ (v₂ :: τ x)][ev ↦ (v₂ :: (τ ev).tail)]τ) ev).head! = v₂ := by
     simp [‹x ≠ ev›, List.head!]
   rw [for_dec head_ev head_x, sub_self]
-  have : (([x ↦ (v₂ :: τ x)][ev ↦ (v₂ :: (τ ev).tail!)]τ) ev).tail! = (τ ev).tail! := by
+  have : (([x ↦ (v₂ :: τ x)][ev ↦ (v₂ :: (τ ev).tail)]τ) ev).tail = (τ ev).tail := by
     { simp [‹x ≠ ev›] }; rw [this]
-  have : ([ev ↦ (0 :: (τ ev).tail!)][x ↦ (v₂ :: τ x)][ev ↦ (v₂ :: (τ ev).tail!)]τ) ev = 0 :: (τ ev).tail! := by
+  have : ([ev ↦ (0 :: (τ ev).tail)][x ↦ (v₂ :: τ x)][ev ↦ (v₂ :: (τ ev).tail)]τ) ev = 0 :: (τ ev).tail := by
     { simp }; rw [this]
-  have : τ ev ≠ [] := sorry
-  rewrite (config := {occs := .pos [1]}) [← List.cons_head!_tail ‹τ ev ≠ []›]
-  sorry
+  have : τ ev ≠ [] := by
+    sorry -- TODO: trovare modo per dimostrare
+  rw [← ‹(τ ev).head! = 0›, List.cons_head!_tail ‹τ ev ≠ []›]
 
 lemma ev_zero {x y ev : ident} {τ : SCORE.store} : x ≠ ev → y ≠ ev → (τ x).head! = v₁ → (τ y).head! = v₂ → (τ ev).head! = 0 → ((eval (L2S' ev (LOOP.com.ASN x y)) τ) ev).head! = 0 := by
   intros
