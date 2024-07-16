299de4f990a29378ba1db630a7cbe73787d12078
andreadlm <delmastro2000@gmail.com>
2024-07-16 15:02:27 +0200

    Nuova definizione di state come tipo induttivo. Riformulazione interprete

--- MasterThesis/SCORE/Interpreter.lean
+++ MasterThesis/SCORE/Interpreter.lean
@@ -1,52 +1,57 @@
+import Mathlib.Logic.Function.Iterate
+
 import MasterThesis.SCORE.Language
 import MasterThesis.SCORE.Functions
 
 namespace SCORE
 
-open SCORE.com
+open SCORE.com SCORE.store SCORE.state
 
 mutual
-  def eval (P : com) (τ : store) : store :=
-    match P with
-    | SKIP    => τ
-    | CON x   => [x ↦ (0 :: (τ x))] τ
-    | NOC x   => match τ x with
-                 | 0 :: t => [x ↦ t] τ
-                 | _      => τ -- TODO: error state
-    | DEC x   => match (τ x).head? with
-                 | some v => [x ↦ ((v - 1) :: (τ x).tail)] τ
-                 | none   => τ
-    | INC x   => match (τ x).head? with
-                 | some v => [x ↦ ((v + 1) :: (τ x).tail)] τ
-                 | none   => τ -- TODO: error state
-    | SEQ P Q => (eval Q) (eval P τ)
-    | FOR x P => match (τ x).head? with
-                 | some v => match v with
-                             | Int.ofNat   k => (fun τ' => eval P τ' )^[k] τ
-                             | Int.negSucc k => (fun τ' => evalI P τ')^[k.succ] τ
-                 | none   => τ -- TODO: error state
-
-  def evalI (P : com) (τ : store) : store :=
-    match P with
-    | SKIP    => τ
-    | CON x   => match τ x with
-                 | 0 :: t => [x ↦ t] τ
-                 | _      => τ -- TODO: error state
-    | NOC x   => [x ↦ (0 :: (τ x))] τ
-    | DEC x   =>  match (τ x).head? with
-                 | some v => [x ↦ ((v + 1) :: (τ x).tail)] τ
-                 | none   => τ -- TODO: error state
-    | INC x   => match (τ x).head? with
-                 | some v => [x ↦ ((v - 1) :: (τ x).tail)] τ
-                 | none   => τ -- TODO: error state
-    | SEQ P Q => (evalI Q) (evalI P τ)
-    | FOR x P => match (τ x).head? with
-                 | some v => match v with
-                             | Int.ofNat   k => (fun τ' => evalI P τ')^[k] τ
-                             | Int.negSucc k => (fun τ' => eval P τ' )^[k.succ] τ
-                 | none   => τ -- TODO: error state
+  def eval (P : com) (s : state) : state :=
+    match s with
+    | fail       => fail
+    | progress σ =>
+      match P with
+      | SKIP    => s
+      | CON x   => progress ([x ↦ (0 :: (σ x))] σ)
+      | NOC x   => match (σ x).head? with
+                   | some 0 => progress ([x ↦ (σ x).tail] σ)
+                   | _      => fail
+      | DEC x   => match (σ x).head? with
+                   | some v => progress ([x ↦ ((v - 1) :: (σ x).tail)] σ)
+                   | none   => fail
+      | INC x   => match (σ x).head? with
+                   | some v => progress ([x ↦ ((v + 1) :: (σ x).tail)] σ)
+                   | none   => fail
+      | SEQ P Q => (eval Q) (eval P s)
+      | FOR x P => match (σ x).head? with
+                   | some v => match v with
+                               | Int.ofNat   k => (fun τ => eval  P τ)^[k] s
+                               | Int.negSucc k => (fun τ => evalI P τ)^[k.succ] s
+                   | none   => fail
+  def evalI (P : com) (s : state) : state :=
+    match s with
+    | fail       => fail
+    | progress σ =>
+      match P with
+      | SKIP    => s
+      | CON x   => match (σ x).head? with
+                   | some 0 => progress ([x ↦ (σ x).tail] σ)
+                   | _   => fail
+      | NOC x   => progress ([x ↦ (0 :: (σ x))] σ)
+      | DEC x   => match (σ x).head? with
+                   | some v => progress ([x ↦ ((v + 1) :: (σ x).tail)] σ)
+                   | none   => fail
+      | INC x   => match (σ x).head? with
+                   | some v => progress ([x ↦ ((v - 1) :: (σ x).tail)] σ)
+                   | none   => fail
+      | SEQ P Q => (evalI Q) (evalI P s)
+      | FOR x P => match (σ x).head? with
+                   | some v => match v with
+                               | Int.ofNat   k => (fun τ => evalI P τ)^[k] s
+                               | Int.negSucc k => (fun τ => eval  P τ)^[k.succ] s
+                   | none   => fail
 end
-
-theorem inv_evalI (P : com) (τ : store) : eval (inv P) τ = evalI P τ := sorry
 
 end SCORE
