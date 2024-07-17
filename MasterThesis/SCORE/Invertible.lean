2c56985331fcf915a6eb7962b70708ec6893c1b0
andreadlm <delmastro2000@gmail.com>
2024-07-17 12:40:33 +0200

    Dimostrazione invertibilità casi DEC e INC

--- MasterThesis/SCORE/Invertible.lean
+++ MasterThesis/SCORE/Invertible.lean
@@ -105,3 +105,80 @@ lemma inv_NOC {p q : state} {x : ident} : (eval (NOC x) p) = q ∧ q ≠ ⊥ ↔
     { rw [inv] }; rw [this]
   rw [inv]
   exact inv_CON.symm
+
+lemma inv_DEC {p q : state} {x : ident} : (eval (DEC x) p) = q ∧ q ≠ ⊥ ↔ (eval (DEC x)⁻¹ q) = p ∧ p ≠ ⊥ := by
+  constructor
+  case mp  =>
+    intro
+    have ⟨lh, _⟩ := ‹eval (DEC x) p = q ∧ q ≠ ⊥›
+    clear ‹eval (DEC x) p = q ∧ q ≠ ⊥›
+    match p with
+    | prog σ =>
+      constructor
+      case left =>
+        rw [eval] at ‹eval (DEC x) (prog σ) = q›
+        split at lh
+        case h_1 v _ =>
+          rw [← ‹prog ([x ↦ ((v - 1) :: (σ x).tail)]σ) = q›, inv, eval]
+          have : ([x ↦ ((v - 1) :: (σ x).tail)] σ) x = (v - 1) :: (σ x).tail := by
+            { simp }; rw [this]
+          have : ((v - 1) :: (σ x).tail).head? = some (v - 1) := by
+            { rw [List.head?] }; simp only [this]
+          rw [update_shrink]
+          have : (v - 1 + 1) = v := by
+            { rw [Int.sub_add_cancel] }; rw [this]
+          have : ((v - 1) :: (σ x).tail).tail = (σ x).tail := by
+            { rw [List.tail_cons] }; rw [this]
+          rw [update_unchanged_cons ‹(σ x).head? = some v›]
+        case h_2 =>
+          symm at ‹⊥ = q›
+          contradiction
+      case right =>
+        by_contra
+        simp only [‹prog σ = ⊥›, eval] at ‹eval (DEC x) (prog σ) = q›
+        symm at ‹⊥ = q›
+        contradiction
+    | ⊥      =>
+      rw [eval] at ‹eval (DEC x) ⊥ = q›
+      symm at ‹⊥ = q›
+      contradiction
+  case mpr =>
+    intro
+    have ⟨lh, _⟩ := ‹eval (DEC x)⁻¹ q = p ∧ p ≠ ⊥›
+    clear ‹eval (DEC x)⁻¹ q = p ∧ p ≠ ⊥›
+    match q with
+    | prog σ =>
+      constructor
+      case left =>
+        rw [inv, eval] at ‹eval (DEC x)⁻¹ (prog σ) = p›
+        split at lh
+        case h_1 v _ =>
+          rw [← ‹prog ([x ↦ ((v + 1) :: (σ x).tail)]σ) = p›, eval]
+          have : ([x ↦ ((v + 1) :: (σ x).tail)] σ) x = (v + 1) :: (σ x).tail := by
+            { simp }; rw [this]
+          have : ((v + 1) :: (σ x).tail).head? = some (v + 1) := by
+            { rw [List.head?] }; simp only [this]
+          rw [update_shrink]
+          have : (v + 1 - 1) = v := by
+            { rw [Int.add_sub_cancel] }; rw [this]
+          have : ((v + 1) :: (σ x).tail).tail = (σ x).tail := by
+            { rw [List.tail_cons] }; rw [this]
+          rw [update_unchanged_cons ‹(σ x).head? = some v›]
+        case h_2 =>
+          symm at ‹⊥ = p›
+          contradiction
+      case right =>
+        by_contra
+        simp only [‹prog σ = ⊥›, inv, eval] at ‹eval (DEC x)⁻¹ (prog σ) = p›
+        symm at ‹⊥ = p›
+        contradiction
+    | ⊥ =>
+      rw [inv, eval] at ‹eval (DEC x)⁻¹ ⊥ = p›
+      symm at ‹⊥ = p›
+      contradiction
+
+lemma inv_INC {p q : state} {x : ident} : (eval (INC x) p) = q ∧ q ≠ ⊥ ↔ (eval (INC x)⁻¹ q) = p ∧ p ≠ ⊥ := by
+  have : (INC x) = (DEC x)⁻¹ := by
+    { rw [inv] }; rw [this]
+  rw [inv]
+  exact inv_DEC.symm
