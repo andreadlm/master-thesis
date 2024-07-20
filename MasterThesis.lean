3cbe4749ff62b0d6b4ec0dd34521bf2d758cbe97
andreadlm <delmastro2000@gmail.com>
2024-07-20 21:30:51 +0200

    Struttura del progetto

--- MasterThesis.lean
+++ MasterThesis.lean
@@ -1,2 +1,4 @@
 -- This module serves as the root of the `MasterThesis` library.
 -- Import modules here that should be built as part of the library.
+import MasterThesis.SCORE.Language
+import MasterThesis.LOOP.Language
