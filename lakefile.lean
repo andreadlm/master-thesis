d33c424462dc26265f7f2995302ce4875062db10
andreadlm <delmastro2000@gmail.com>
2024-08-04 18:06:51 +0200

    Tool di creazione della documentazione, aggiornamento a lean 4.10.0

--- lakefile.lean
+++ lakefile.lean
@@ -2,14 +2,14 @@ import Lake
 open Lake DSL
 
 package «master_thesis» where
-  -- Settings applied to both builds and interactive editing
   leanOptions := #[
     ⟨`pp.unicode.fun, true⟩ -- pretty-prints `fun a ↦ b`
   ]
-  -- add any additional package configuration options here
 
-require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git" @ "stable"
+require mathlib from git "https://github.com/leanprover-community/mathlib4.git" @ "v4.10.0"
+
+meta if get_config? doc = some "on" then
+require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "v4.10.0"
 
 @[default_target]
 lean_lib «MasterThesis» where
