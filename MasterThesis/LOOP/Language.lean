345743f15f63ac359e2769f1d1e102f069ce8c80
andreadlm <delmastro2000@gmail.com>
2024-08-04 14:29:28 +0200

    Divisione definizioni e prove per avere indipendenza rispetto a Mathlib alla compilazione dell'eseguibile

--- MasterThesis/LOOP/Language.lean
+++ MasterThesis/LOOP/Language.lean
@@ -1,6 +1,3 @@
-import Mathlib.Tactic.Basic
-import Mathlib.Logic.Basic
-
 import MasterThesis.Commons
 
 namespace LOOP
@@ -16,26 +13,6 @@ def update (x : Ident) (v : Nat) (s : Store) : Store :=
 
 notation:100 "[" x:100 " ↦ " v:100 "]" s:100 => update x v s -- Migliorare?
 notation:100 "[" x:100 " ↦ " v:100 "]"       => [x ↦ v] emp -- Migliorare?
-
-@[simp]
-lemma update_same {s : Store} {x y : Ident} {v : Nat} : x = y → (update x v s) y = v := by
-  intros
-  unfold update
-  apply if_pos
-  assumption
-
-@[simp]
-lemma update_other {s : Store} {x y : Ident} {v : Nat} : x ≠ y → (update x v s) y = (s y) := by
-  intros
-  unfold update
-  apply if_neg
-  assumption
-
-lemma update_no_update {s : Store} {x : Ident} : (update x (s x) s) = s := by
-  funext y
-  cases eq_or_ne x y with
-  | inl /- x = y -/ => rw[update_same ‹x = y›, ‹x = y›]
-  | inr /- x ≠ y -/ => rw[update_other ‹x ≠ y›]
 
 end Store
 
---
+++ MasterThesis/LOOP/Proofs/Language.lean
@@ -0,0 +1,26 @@
+import Mathlib.Tactic.Basic
+import Mathlib.Logic.Basic
+
+import MasterThesis.LOOP.Language
+
+open LOOP Store
+
+@[simp]
+lemma update_same {s : Store} {x y : Ident} {v : Nat} : x = y → (update x v s) y = v := by
+  intros
+  unfold update
+  apply if_pos
+  assumption
+
+@[simp]
+lemma update_other {s : Store} {x y : Ident} {v : Nat} : x ≠ y → (update x v s) y = (s y) := by
+  intros
+  unfold update
+  apply if_neg
+  assumption
+
+lemma update_no_update {s : Store} {x : Ident} : (update x (s x) s) = s := by
+  funext y
+  cases eq_or_ne x y with
+  | inl /- x = y -/ => rw[update_same ‹x = y›, ‹x = y›]
+  | inr /- x ≠ y -/ => rw[update_other ‹x ≠ y›]
