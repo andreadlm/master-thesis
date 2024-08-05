076a788dd507bb3adc5dc7b4297158575d05fff5
andreadlm <delmastro2000@gmail.com>
2024-08-05 12:31:33 +0200

    Import di tutti i file che compilano

--- MasterThesis.lean
+++ MasterThesis.lean
@@ -1,5 +1,13 @@
--- This module serves as the root of the `MasterThesis` library.
--- Import modules here that should be built as part of the library.
-import MasterThesis.SCORE.Language
-import MasterThesis.LOOP.Language
 import MasterThesis.Commons
+import MasterThesis.SCORE.Language
+import MasterThesis.SCORE.Interpreter
+import MasterThesis.SCORE.Compiler_v1
+import MasterThesis.SCORE.Compiler_v2
+-- import MasterThesis.SCORE.Proofs.Language
+-- import MasterThesis.SCORE.Proofs.Invertible
+-- import MasterThesis.SCORE.Proofs.Compiler_v1
+-- import MasterThesis.SCORE.Proofs.Compiler_v2
+import MasterThesis.LOOP.Language
+import MasterThesis.LOOP.Interpreter
+import MasterThesis.LOOP.Parser
+import MasterThesis.LOOP.Proofs.Language
