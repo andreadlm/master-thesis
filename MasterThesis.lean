ec7837201945aaeac1e45b193aea392c3d56f9ca
andreadlm <delmastro2000@gmail.com>
2024-08-07 18:26:46 +0200

    Modificati sorgenti da includere nella compilazione

--- MasterThesis.lean
+++ MasterThesis.lean
@@ -1,13 +1,13 @@
 import MasterThesis.Commons
 import MasterThesis.SCORE.Language
 import MasterThesis.SCORE.Interpreter
-import MasterThesis.SCORE.Compiler_v1
-import MasterThesis.SCORE.Compiler_v2
 import MasterThesis.SCORE.Proofs.Language
 import MasterThesis.SCORE.Proofs.Invertible
--- import MasterThesis.SCORE.Proofs.Compiler_v1
--- import MasterThesis.SCORE.Proofs.Compiler_v2
 import MasterThesis.LOOP.Language
 import MasterThesis.LOOP.Interpreter
 import MasterThesis.LOOP.Parser
 import MasterThesis.LOOP.Proofs.Language
+import MasterThesis.Compiler.Compiler_v1
+import MasterThesis.Compiler.Compiler_v2
+import MasterThesis.Compiler.Proofs.Compiler_v1
+-- import MasterThesis.Compiler.Proofs.Compiler_v2
