38842fc0ea6a655c7ba7ec69f2104b98154cf28b
andreadlm <delmastro2000@gmail.com>
2024-08-10 17:30:13 +0200

    Dimostrazione lemma for_inc

--- MasterThesis/Compiler/Proofs/Compiler_v2.lean
+++ MasterThesis/Compiler/Proofs/Compiler_v2.lean
@@ -10,37 +10,59 @@ namespace l2s'
 open SCORE Com
 open LOOP Com
 
-lemma iter_inc {x : Ident} {σ : SCORE.Store} {k : Int} (v : ℕ) : (σ x).head? = some k → (fun τ ↦ SCORE.eval (INC x) τ)^[v] σ = [x ↦ (k + ↑v) :: (σ x).tail] σ := by
+lemma iter_inc {x : Ident} {σ : SCORE.Store} {k : Int} (v : ℕ) : (σ x).head? = some k → (fun t ↦ SCORE.eval (INC x) t)^[v] σ = [x ↦ (k + ↑v) :: (σ x).tail] σ := by
   intro
   induction v
   case zero =>
     simp [Store.update_unchanged_cons ‹(σ x).head? = k›]
   case succ m ih =>
     calc
-      (fun τ ↦ SCORE.eval (INC x) τ)^[m + 1] σ
-      _ = eval (INC x) ((fun τ ↦ SCORE.eval (INC x) τ)^[m] σ) := by simp [Nat.add_comm m 1, Function.iterate_add_apply]
+      (fun t => SCORE.eval (INC x) t)^[m + 1] σ
+      _ = eval (INC x) ((fun t => SCORE.eval (INC x) t)^[m] σ) := by simp [Nat.add_comm m 1, Function.iterate_add_apply]
       _ = eval (INC x) ([x ↦ (k + ↑m) :: (σ x).tail] σ)       := by rw [ih]
       _ = [x ↦ (k + ↑m + 1) :: (σ x).tail] σ                  := by simp [SCORE.eval, ‹(σ x).head? = some k›]
       _ = [x ↦ (k + ↑(m + 1)) :: (σ x).tail] σ                := by
         have : k + ↑m + 1 = k + (↑m + 1) := by linarith
         simp [this]
 
-lemma iter_dec {x : Ident} {σ : SCORE.Store} {k : Int} (v : ℕ) : (σ x).head? = some k → (fun τ ↦ SCORE.eval (DEC x) τ)^[v] σ = [x ↦ (k - ↑v) :: (σ x).tail] σ := by
+lemma iter_dec {x : Ident} {σ : SCORE.Store} {k : Int} (v : ℕ) : (σ x).head? = some k → (fun t ↦ SCORE.eval (DEC x) t)^[v] σ = [x ↦ (k - ↑v) :: (σ x).tail] σ := by
   intro
   induction v
   case zero =>
     simp [Store.update_unchanged_cons ‹(σ x).head? = k›]
   case succ m ih =>
     calc
-      (fun τ ↦ SCORE.eval (DEC x) τ)^[m + 1] σ
-      _ = eval (DEC x) ((fun τ ↦ SCORE.eval (DEC x) τ)^[m] σ) := by simp [Nat.add_comm m 1, Function.iterate_add_apply]
+      (fun t => SCORE.eval (DEC x) t)^[m + 1] σ
+      _ = eval (DEC x) ((fun t => SCORE.eval (DEC x) t)^[m] σ) := by simp [Nat.add_comm m 1, Function.iterate_add_apply]
       _ = eval (DEC x) ([x ↦ (k - ↑m) :: (σ x).tail] σ)       := by rw [ih]
       _ = [x ↦ (k - ↑m - 1) :: (σ x).tail] σ                  := by simp [SCORE.eval, ‹(σ x).head? = some k›]
       _ = [x ↦ (k - ↑(m + 1)) :: (σ x).tail] σ                := by
         have : k - ↑m - 1 = k - (↑m + 1) := by linarith
         simp [this]
 
-lemma for_inc {x y : Ident} {v₁ v₂ : Int} {σ : SCORE.Store} : (σ x).head? = v₁ → (σ y).head? = v₂ → SCORE.eval (FOR y (INC x)) σ = [x ↦ (v₁ + v₂) :: (σ x).tail] σ := by sorry
+lemma for_inc {x y : Ident} {v₁ v₂ : Int} {σ : SCORE.Store} : (σ x).head? = v₁ → (σ y).head? = v₂ → SCORE.eval (FOR y (INC x)) σ = [x ↦ (v₁ + v₂) :: (σ x).tail] σ := by
+  intros
+  rw [SCORE.eval]
+  split
+  · split
+    case _ k _ =>
+      calc
+        (fun t => SCORE.eval (INC x) t)^[k] σ
+        _ = [x ↦ (v₁ + ↑k) :: (σ x).tail] σ := iter_inc k ‹(σ x).head? = some v₁›
+        _ = [x ↦ (v₁ + v₂) :: (σ x).tail] σ := by simp [Option.some.inj (Eq.trans ‹(σ y).head? = some v₂›.symm ‹(σ y).head? = some (Int.ofNat k)›)]
+    case _ k _ =>
+      have evalI_INC_eq_eval_DEC {x : Ident} {s : SCORE.State} : evalI (INC x) s = eval (DEC x) s := by
+        cases Option.eq_none_or_eq_some s
+        · rw [‹s = none›, SCORE.evalI, SCORE.eval]
+        · cases ‹∃ x, s = some x› with
+          | intro σ _ => rw [‹s = some σ›, SCORE.evalI, SCORE.eval]
+      calc
+        (fun t => SCORE.evalI (INC x) t)^[k + 1] σ
+        _ = (fun t => SCORE.eval (DEC x) t)^[k + 1] σ := by simp only [evalI_INC_eq_eval_DEC]
+        _ = [x ↦ (v₁ - ↑(k + 1)) :: (σ x).tail] σ     := by exact iter_dec (k + 1) ‹(σ x).head? = some v₁›
+        _ = [x ↦ (v₁ + v₂) :: (σ x).tail] σ           := by sorry
+  · rw [‹(σ y).head? = some v₂›] at ‹(σ y).head? = none›
+    contradiction
 
 lemma for_dec {x y : Ident} {v₁ v₂ : Int} {σ : SCORE.Store} : (σ x).head? = v₁ → (σ y).head? = v₂ → SCORE.eval (FOR y (DEC x)) σ = [x ↦ (v₁ - v₂) :: (σ x).tail] σ := by sorry
 
