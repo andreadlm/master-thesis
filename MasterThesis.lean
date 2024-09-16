65edcf7f38c7235470d043e006474057c2528183
andreadlm <delmastro2000@gmail.com>
2024-09-16 15:23:39 +0200

    Rimossa seconda versione compilatore per ora non completa

--- MasterThesis.lean
+++ MasterThesis.lean
@@ -4,10 +4,9 @@ import MasterThesis.SCORE.Interpreter
 import MasterThesis.SCORE.Proofs.Language
 import MasterThesis.SCORE.Proofs.Invertible
 import MasterThesis.LOOP.Language
-import MasterThesis.LOOP.Interpreter
 import MasterThesis.LOOP.Parser
 import MasterThesis.LOOP.Proofs.Language
 import MasterThesis.Compiler.Compiler_v1
-import MasterThesis.Compiler.Compiler_v2
+--import MasterThesis.Compiler.Compiler_v2
 import MasterThesis.Compiler.Proofs.Compiler_v1
-import MasterThesis.Compiler.Proofs.Compiler_v2
+--import MasterThesis.Compiler.Proofs.Compiler_v2
