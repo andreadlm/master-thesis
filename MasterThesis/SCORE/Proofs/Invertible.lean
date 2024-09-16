1b1ac7b414108ea0ea9bd6ecac5302d377fff445
andreadlm <delmastro2000@gmail.com>
2024-09-16 15:22:49 +0200

    Semplificate le dimostrazioni

--- MasterThesis/SCORE/Proofs/Invertible.lean
+++ MasterThesis/SCORE/Proofs/Invertible.lean
@@ -5,21 +5,18 @@ open SCORE Com Store State
 
 lemma invertible_SKIP {s t : State} : (eval SKIP s) = t ∧ t ≠ ⊥ ↔ (eval SKIP⁻¹ t) = s ∧ s ≠ ⊥ := by
   constructor
-  case mp  =>
+  case mp =>
     intro
     have ⟨_, _⟩ := ‹eval SKIP s = t ∧ t ≠ ⊥›
     match s with
     | some σ =>
       constructor
-      case left  =>
-        rw [eval] at ‹eval SKIP (some σ) = t›
-        rw [inv, ←‹some σ = t›, eval]
+      case left =>
+        rw [eval] at ‹eval SKIP σ = t›
+        simp [inv, ←‹σ = t›, eval]
       case right =>
-        by_contra
-        simp only [‹some σ = ⊥›, eval] at ‹eval SKIP (some σ) = t›
-        symm at ‹⊥ = t›
-        contradiction
-    | ⊥      =>
+        exact Option.some_ne_none σ
+    | ⊥ =>
       rw [eval] at ‹eval SKIP ⊥ = t›
       symm at ‹⊥ = t›
       contradiction
@@ -29,38 +26,31 @@ lemma invertible_SKIP {s t : State} : (eval SKIP s) = t ∧ t ≠ ⊥ ↔ (eval 
     match t with
       | some σ =>
         constructor
-        case left  =>
-          rw [inv, eval] at ‹eval SKIP⁻¹ (some σ) = s›
-          rw [←‹some σ = s›, eval]
+        case left =>
+          rw [inv, eval] at ‹eval SKIP⁻¹ σ = s›
+          simp [←‹σ = s›, eval]
         case right =>
-          by_contra
-          simp only [‹some σ = ⊥›, eval] at ‹eval SKIP (some σ) = s›
-          symm at ‹⊥ = s›
-          contradiction
-      | ⊥      =>
+          exact Option.some_ne_none σ
+      | ⊥ =>
         rw [inv, eval] at ‹eval SKIP⁻¹ ⊥ = s›
         symm at ‹⊥ = s›
         contradiction
 
 lemma invertible_CON {s t : State} {x : Ident} : (eval (CON x) s) = t ∧ t ≠ ⊥ ↔ (eval (CON x)⁻¹ t) = s ∧ s ≠ ⊥ := by
   constructor
-  case mp  =>
+  case mp =>
     intro
     have ⟨_, _⟩ := ‹eval (CON x) s = t ∧ t ≠ ⊥›
     clear ‹eval (CON x) s = t ∧ t ≠ ⊥›
     match s with
     | some σ =>
       constructor
-      case left  =>
-        rw [eval] at ‹eval (CON x) (some σ) = t›
-        rw [←‹some (σ[x ↦ 0 :: σ x]) = t›, inv, eval]
-        simp
+      case left =>
+        rw [eval] at ‹eval (CON x) σ = t›
+        simp [inv, ←‹σ[x ↦ 0 :: σ x] = t›, eval]
       case right =>
-        by_contra
-        simp only [‹some σ = ⊥›, eval] at ‹eval (CON x) (some σ) = t›
-        symm at ‹⊥ = t›
-        contradiction
-    | ⊥      =>
+        exact Option.some_ne_none σ
+    | ⊥ =>
       rw [eval] at ‹eval (CON x) ⊥ = t›
       symm at ‹⊥ = t›
       contradiction
@@ -71,29 +61,27 @@ lemma invertible_CON {s t : State} {x : Ident} : (eval (CON x) s) = t ∧ t ≠ 
     match t with
     | some σ =>
       constructor
-      case left  =>
-        rw [inv, eval] at ‹eval (CON x)⁻¹ (some σ) = s›
+      case left =>
+        rw [inv, eval] at ‹eval (CON x)⁻¹ σ = s›
         split at lh
         case h_1 =>
-          rw [←‹some (σ[x ↦ (σ x).tail]) = s›, eval]
+          rw [←‹σ[x ↦ (σ x).tail] = s›, eval]
           simp [update_unchanged_cons ‹(σ x).head? = some 0›]
         case h_2 =>
           symm at ‹⊥ = s›
           contradiction
       case right =>
-        by_contra
-        simp only [‹some σ = ⊥›, eval] at ‹eval (CON x)⁻¹ (some σ) = s›
-        symm at ‹⊥ = s›
-        contradiction
-    | ⊥      =>
+        exact Option.some_ne_none σ
+    | ⊥ =>
       rw [inv, eval] at ‹eval (CON x)⁻¹ ⊥ = s›
       symm at ‹⊥ = s›
       contradiction
 
 lemma invertible_NOC {s t : State} {x : Ident} : (eval (NOC x) s) = t ∧ t ≠ ⊥ ↔ (eval (NOC x)⁻¹ t) = s ∧ s ≠ ⊥ := by
-  have : (NOC x) = (CON x)⁻¹ := by rw [inv]
-  rw [this, inv]
-  exact invertible_CON.symm
+  have : NOC x = (CON x)⁻¹ := by rw [inv]
+  rw [‹NOC x = (CON x)⁻¹› , inv]
+  symm
+  exact invertible_CON
 
 lemma invertible_DEC {s t : State} {x : Ident} : (eval (DEC x) s) = t ∧ t ≠ ⊥ ↔ (eval (DEC x)⁻¹ t) = s ∧ s ≠ ⊥ := by
   constructor
@@ -105,19 +93,16 @@ lemma invertible_DEC {s t : State} {x : Ident} : (eval (DEC x) s) = t ∧ t ≠ 
     | some σ =>
       constructor
       case left =>
-        rw [eval] at ‹eval (DEC x) (some σ) = t›
+        rw [eval] at ‹eval (DEC x) σ = t›
         split at lh
         case h_1 v _ =>
-          rw [inv, ←‹some (σ[x ↦ (v - 1) :: (σ x).tail]) = t›, eval]
-          simp [update_unchanged_cons ‹(σ x).head? = some v›]
+          simpa [inv, ←‹σ[x ↦ (v - 1) :: (σ x).tail] = t›, eval]
+            using update_unchanged_cons ‹(σ x).head? = v›
         case h_2 =>
           symm at ‹⊥ = t›
           contradiction
       case right =>
-        by_contra
-        simp only [‹some σ = ⊥›, eval] at ‹eval (DEC x) (some σ) = t›
-        symm at ‹⊥ = t›
-        contradiction
+        exact Option.some_ne_none σ
     | ⊥      =>
       rw [eval] at ‹eval (DEC x) ⊥ = t›
       symm at ‹⊥ = t›
@@ -130,29 +115,26 @@ lemma invertible_DEC {s t : State} {x : Ident} : (eval (DEC x) s) = t ∧ t ≠ 
     | some σ =>
       constructor
       case left =>
-        rw [inv, eval] at ‹eval (DEC x)⁻¹ (some σ) = s›
+        rw [inv, eval] at ‹eval (DEC x)⁻¹ σ = s›
         split at lh
         case h_1 v _ =>
-          rw [←‹some (σ[x ↦ ((v + 1) :: (σ x).tail)]) = s›, eval]
-          simp [update_unchanged_cons ‹(σ x).head? = some v›]
+          simpa [←‹σ[x ↦ ((v + 1) :: (σ x).tail)] = s›, eval]
+            using update_unchanged_cons ‹(σ x).head? = v›
         case h_2 =>
           symm at ‹⊥ = s›
           contradiction
       case right =>
-        by_contra
-        simp only [‹some σ = ⊥›, inv, eval] at ‹eval (DEC x)⁻¹ (some σ) = s›
-        symm at ‹⊥ = s›
-        contradiction
+        exact Option.some_ne_none σ
     | ⊥ =>
       rw [inv, eval] at ‹eval (DEC x)⁻¹ ⊥ = s›
       symm at ‹⊥ = s›
       contradiction
 
 lemma invertible_INC {s t : State} {x : Ident} : (eval (INC x) s) = t ∧ t ≠ ⊥ ↔ (eval (INC x)⁻¹ t) = s ∧ s ≠ ⊥ := by
-  have : (INC x) = (DEC x)⁻¹ := by
-    { rw [inv] }; rw [this]
-  rw [inv]
-  exact invertible_DEC.symm
+  have : INC x = (DEC x)⁻¹ := by rw [inv]
+  rw [‹INC x = (DEC x)⁻¹›, inv]
+  symm
+  exact invertible_DEC
 
 lemma invertible_SEQ {s t : State} {P Q : Com} (ih₁ : ∀ {s t : State}, eval P s = t ∧ t ≠ ⊥ ↔ eval P⁻¹ t = s ∧ s ≠ ⊥) (ih₂ : ∀ {s t : State}, eval Q s = t ∧ t ≠ ⊥ ↔ eval Q⁻¹ t = s ∧ s ≠ ⊥) : (eval (SEQ P Q) s) = t ∧ t ≠ ⊥ ↔ (eval (SEQ P Q)⁻¹ t) = s ∧ s ≠ ⊥ := by
   constructor
@@ -163,15 +145,15 @@ lemma invertible_SEQ {s t : State} {P Q : Com} (ih₁ : ∀ {s t : State}, eval 
     match s, t with
     | some σ, some τ =>
       rw [inv, eval]
-      rw [eval] at ‹eval (P ;; Q) (some σ) = some τ›
-      have ⟨_, _⟩ := ih₂.mp ⟨‹eval Q (eval P (some σ)) = some τ›, ‹some τ ≠ ⊥›⟩
-      have : eval Q⁻¹ (some τ) ≠ ⊥ := Eq.trans_ne ‹eval Q⁻¹ (some τ) = eval P (some σ)› ‹eval P (some σ) ≠ ⊥›
-      exact ih₁.mp ⟨‹eval Q⁻¹ (some τ) = eval P (some σ)›.symm, ‹eval Q⁻¹ (some τ) ≠ ⊥›⟩
-    | ⊥     , t      =>
+      rw [eval] at ‹eval (P ;; Q) σ = τ›
+      have ⟨_, _⟩ := ih₂.mp ⟨‹eval Q (eval P σ) = τ›, ‹some τ ≠ ⊥›⟩
+      have : eval Q⁻¹ τ ≠ ⊥ := Eq.trans_ne ‹eval Q⁻¹ τ = eval P σ› ‹eval P σ ≠ ⊥›
+      exact ih₁.mp ⟨‹eval Q⁻¹ τ = eval P σ›.symm, ‹eval Q⁻¹ τ ≠ ⊥›⟩
+    | ⊥, t =>
       rw [eval] at ‹eval (P ;; Q) ⊥ = t›
       symm at ‹⊥ = t›
       contradiction
-    | s     , ⊥     =>
+    | s, ⊥ =>
       contradiction
   case mpr =>
     intro
@@ -180,13 +162,13 @@ lemma invertible_SEQ {s t : State} {P Q : Com} (ih₁ : ∀ {s t : State}, eval 
     match t, s with
     | some τ, some σ =>
       rw [eval]
-      rw [inv, eval] at ‹eval (P ;; Q)⁻¹ (some τ) = some σ›
-      have ⟨_, _⟩ := ih₁.mpr ⟨‹eval P⁻¹ (eval Q⁻¹ (some τ)) = some σ›, ‹some σ ≠ ⊥›⟩
-      have : eval P (some σ) ≠ ⊥ := Eq.trans_ne ‹eval P (some σ) = eval Q⁻¹ (some τ)› ‹eval Q⁻¹ (some τ) ≠ ⊥›
-      exact ih₂.mpr ⟨‹eval P (some σ) = eval Q⁻¹ (some τ)›.symm, ‹eval P (some σ) ≠ ⊥›⟩
-    | t     , ⊥      =>
+      rw [inv, eval] at ‹eval (P ;; Q)⁻¹ τ = σ›
+      have ⟨_, _⟩ := ih₁.mpr ⟨‹eval P⁻¹ (eval Q⁻¹ τ) = σ›, ‹some σ ≠ ⊥›⟩
+      have : eval P σ ≠ ⊥ := Eq.trans_ne ‹eval P σ = eval Q⁻¹ τ› ‹eval Q⁻¹ τ ≠ ⊥›
+      exact ih₂.mpr ⟨‹eval P σ = eval Q⁻¹ τ›.symm, ‹eval P σ ≠ ⊥›⟩
+    | t, ⊥ =>
       contradiction
-    | ⊥     , s      =>
+    | ⊥, s =>
       rw [inv, eval] at ‹eval (P ;; Q)⁻¹ ⊥ = s›
       symm at ‹⊥ = s›
       contradiction
