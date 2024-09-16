8c50f4a478be13c041d5c1c33dcdab8a179b9fc6
andreadlm <delmastro2000@gmail.com>
2024-09-16 15:22:36 +0200

    Snellita la notazione

--- MasterThesis/SCORE/Interpreter.lean
+++ MasterThesis/SCORE/Interpreter.lean
@@ -8,48 +8,48 @@ open SCORE Com
 mutual
   def eval (P : Com) (s : State) : State :=
     match s with
-    | none   => none
+    | ⊥      => ⊥
     | some σ =>
       match P with
       | SKIP    => s
-      | CON x   => some (σ [x ↦ (0 :: (σ x))])
+      | CON x   => σ[x ↦ (0 :: (σ x))]
       | NOC x   => match (σ x).head? with
-                   | some 0 => some (σ [x ↦ (σ x).tail])
-                   | _      => none
+                   | some 0 => σ[x ↦ (σ x).tail]
+                   | _      => ⊥
       | DEC x   => match (σ x).head? with
-                   | some v => some (σ [x ↦ ((v - 1) :: (σ x).tail)])
-                   | none   => none
+                   | some v => σ[x ↦ ((v - 1) :: (σ x).tail)]
+                   | none   => ⊥
       | INC x   => match (σ x).head? with
-                   | some v => some (σ [x ↦ ((v + 1) :: (σ x).tail)])
-                   | none   => none
+                   | some v => σ[x ↦ ((v + 1) :: (σ x).tail)]
+                   | none   => ⊥
       | SEQ P Q => (eval Q) (eval P s)
       | FOR x P => match (σ x).head? with
                    | some v => match v with
                                | Int.ofNat   k => (fun t => eval  P t)^[k] s
                                | Int.negSucc k => (fun t => evalI P t)^[k.succ] s
-                   | none   => none
+                   | none   => ⊥
   def evalI (P : Com) (s : State) : State :=
     match s with
-    | none   => none
+    | ⊥      => ⊥
     | some σ =>
       match P with
       | SKIP    => s
       | CON x   => match (σ x).head? with
-                   | some 0 => some (σ [x ↦ (σ x).tail])
-                   | _   => none
-      | NOC x   => some (σ [x ↦ (0 :: (σ x))])
+                   | some 0 => σ[x ↦ (σ x).tail]
+                   | _   => ⊥
+      | NOC x   => σ [x ↦ (0 :: (σ x))]
       | DEC x   => match (σ x).head? with
-                   | some v => some (σ [x ↦ ((v + 1) :: (σ x).tail)])
-                   | none   => none
+                   | some v => σ[x ↦ ((v + 1) :: (σ x).tail)]
+                   | none   => ⊥
       | INC x   => match (σ x).head? with
-                   | some v => some (σ [x ↦ ((v - 1) :: (σ x).tail)])
-                   | none   => none
+                   | some v => σ[x ↦ ((v - 1) :: (σ x).tail)]
+                   | none   => ⊥
       | SEQ P Q => (evalI Q) (evalI P s)
       | FOR x P => match (σ x).head? with
                    | some v => match v with
                                | Int.ofNat   k => (fun t => evalI P t)^[k] s
                                | Int.negSucc k => (fun t => eval  P t)^[k.succ] s
-                   | none   => none
+                   | none   => ⊥
 end
 
 end SCORE
