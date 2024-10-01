4a91669dbdbe504ef8e481f7c607281fb2637737
andreadlm <delmastro2000@gmail.com>
2024-10-01 11:06:38 +0200

    Aggiunto compiler_v2

--- MasterThesis.lean
+++ MasterThesis.lean
@@ -3,4 +3,4 @@ import MasterThesis.SCORE.Language
 import MasterThesis.LOOP.Language
 import MasterThesis.LOOP.Parser
 import MasterThesis.Compiler.Compiler_v1
---import MasterThesis.Compiler.Compiler_v2
+import MasterThesis.Compiler.Compiler_v2
